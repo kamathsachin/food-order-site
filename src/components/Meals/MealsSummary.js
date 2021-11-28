53005c5d9851029c3221a4ee6ee65d7dc07ed689
Sachin Kamath <kamathsachin@hotmail.com>
2021-11-27 19:02:10 -0600

    Implemented Meals Component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut placeat
+        accusantium veritatis nobis numquam provident? Delectus similique
+        voluptate sunt, quos quis reiciendis dignissimos et dolore.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+        modi accusantium, numquam exercitationem molestiae architecto
+        reprehenderit magni consequatur asperiores aut blanditiis cumque
+        delectus tempore eveniet id itaque possimus veniam quos!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
