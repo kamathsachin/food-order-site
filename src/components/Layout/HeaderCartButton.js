28a4f5831c57f6fa04dfebf0e9027b52af75c57c
Sachin Kamath <kamathsachin@hotmail.com>
2021-12-02 18:25:54 -0600

    Outputting cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/Cart-Context';
 import CartIcon from '../Cart/CartIcon';
 
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const noOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3.33</span>
+      <span className={classes.badge}>{noOfCartItems}</span>
     </button>
   );
 };
